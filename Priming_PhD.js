9fa2cd79cd2a90671bd49c582e17ac90cae46ad1
icense-data <65609366+icense-data@users.noreply.github.com>
2021-12-13 10:57:10 +0100

    Update Priming_PhD.js

--- Priming_PhD.js
+++ Priming_PhD.js
@@ -11,14 +11,14 @@ define(['pipAPI', 'https://icense-data.github.io/SC-IAT/Priming_Full_Script.js']
 					mediaArray : [
     				        {word : 'I like teaching'}, 
     					{word : 'I like statistics'}, 
-    					{word : 'I like learning'}, 
+    					{word : 'I can talk clearly about my needs'}, 
     					{word : 'I like reading scientific papers.'}, 
     					{word : 'I want to become a professor'},
                                         {word : 'I like to read about social sciences.'},
 					{word : 'I am very ambitious'}, 
-    					//{word : 'I do not give up easily'}, 
-    					//{word : 'I am looking forward to start the PhD'}, 
-    					//{word : 'I like science'}, 
+    					{word : 'I do not give up easily'}, 
+    					{word : 'I am looking forward to start a PhD'}, 
+    					{word : 'I can work independently'}, 
     					//{word : 'I like writing'},
                                         //{word : 'I like to study hard'}.
 				    ]
@@ -34,19 +34,19 @@ define(['pipAPI', 'https://icense-data.github.io/SC-IAT/Priming_Full_Script.js']
     					{word : 'I work for the NASA'},
               				{word : 'I am 97 years old'},
 					{word : 'I am looking at a computer screen'}, 
-    					//{word : 'I am having lunch'}, 
-    					//{word : 'I work for the NASA'}, 
-    					//{word : 'I am using a keyboard'}, 
+    					{word : 'I am having lunch'}, 
+    					{word : 'I am using a keyboard'}, 
+    					{word : 'I am running a marathon'}, 
     					//{word : 'I am 100 years old'},
               				//{word : 'I am running the marathon'}.
     				]
 				}
 			],	
 
-			nTrialsPerPrimeTargetPair:3, //How many trials in a block, per prime-target combination (always three blocks).
+			nTrialsPerPrimeTargetPair:10, //How many trials in a block, per prime-target combination (always three blocks).
 			fixationDuration : 500, 
-			errorFBDuration : 1500, 
-			ITIDuration : 400,
+			errorFBDuration : 1000, 
+			ITIDuration : 300,
 
 			//Set the image folder here.
 			base_url : {
